a60c340feb6fe2a56a2316de0240472717165d1a
PetrMachacka <91247837+PetrMachacka@users.noreply.github.com>
2023-01-11 18:33:23 +0100

    Add project files.

---
+++ Morseovka/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Morseovka
+{
+
+    class MorseCodeConverter
+    {
+        private Dictionary<string, string> _morseCodeDictionary = new Dictionary<string, string>
+
+    {
+        {"A", ".-"}, {"B", "-..."}, {"C", "-.-."}, {"D", "-.."}, {"E", "."},
+        {"F", "..-."}, {"G", "--."}, {"H", "...."}, {"I", ".."}, {"J", ".---"},
+        {"K", "-.-"}, {"L", ".-.."}, {"M", "--"}, {"N", "-."}, {"O", "---"},
+        {"P", ".--."}, {"Q", "--.-"}, {"R", ".-."}, {"S", "..."}, {"T", "-"},
+        {"U", "..-"}, {"V", "...-"}, {"W", ".--"}, {"X", "-..-"}, {"Y", "-.--"},
+        {"Z", "--.."},
+        {"0", "-----"}, {"1", ".----"}, {"2", "..---"}, {"3", "...--"},
+        {"4", "....-"}, {"5", "....."}, {"6", "-...."}, {"7", "--..."},
+        {"8", "---.."}, {"9", "----."},
+        {"@", ".--.-."}, {" ", ""}, {"CH", "----"}
+    };
+
+        public string normalize(string txt) 
+        {
+            string normalizedText = txt.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+            foreach (var x in normalizedText)
+            {
+                if (System.Globalization.CharUnicodeInfo.GetUnicodeCategory(x) != System.Globalization.UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(x);
+                }
+            }
+        return sb.ToString();
+    }
+
+        public string Encode(string text)
+        {
+            text = text.Normalize();
+            text = text.ToUpper();
+            Console.WriteLine(text);
+            var encodedText = new StringBuilder();
+
+            foreach (var character in text)
+            {
+                
+                    encodedText.Append(_morseCodeDictionary[character.ToString()] + "/");
+            }
+
+            return encodedText.ToString().TrimEnd();
+        }
+
+        public string Decode(string code)
+        {
+            var decodedText = new StringBuilder();
+            var morseCodeSymbols = code.Split("/");
+
+            var morseCodeDictionaryReversed = _morseCodeDictionary.ToDictionary(x => x.Value, x => x.Key);
+            foreach (var symbol in morseCodeSymbols)
+            {
+                decodedText.Append(morseCodeDictionaryReversed[symbol]);
+            }
+
+            return decodedText.ToString();
+        }
+    }
+}
---
+++ Morseovka/Program.cs
@@ -0,0 +1,53 @@
+﻿using Morseovka;
+using System.Text;
+/*
+Console.WriteLine("Hello, World!");
+char[] source1 = new char[] { 'A', 'B', 'C' };
+string result1 = new string(source);
+​
+// rozdělení řetězce do znaků
+string source2 = "Textový řetězec";
+char[] result2 = source2.ToCharArray();
+​
+// překódování z UTF-8 do ASCII (nefunguje tak, jak bychom potřebovali)
+Encoding srcEnc = Encoding.UTF8;
+Encoding dstEnc = Encoding.ASCII;
+string txt = "Takový hezoučký den.";
+​
+var srcBytes = srcEnc.GetBytes(txt);
+var dstBytes = Encoding.Convert(srcEnc, dstEnc, srcBytes);
+string des = dstEnc.GetString(dstBytes);
+Console.WriteLine(des);
+​
+// odstranění diakritiky
+string txt = "Šílená čivava";
+string normalizedText = txt.Normalize(NormalizationForm.FormD);
+StringBuilder sb = new StringBuilder();
+foreach (var x in normalizedText)
+{
+    if (System.Globalization.CharUnicodeInfo.GetUnicodeCategory(x) != System.Globalization.UnicodeCategory.NonSpacingMark)
+    {
+        sb.Append(x);
+    }
+}
+Console.WriteLine(sb.ToString().Normalize(NormalizationForm.FormC));
+​
+// rozdělení řetězce na podřetězce podle určitého znaku
+string[] symbols = txt.Split('/');
+​
+// spojení znaků do řetězce s využitím oddělovače
+string[] symbols = new string[] { "...", "---", "..." };
+string res = string.Join("/", symbols);
+Console.WriteLine(res);
+​
+// převod znaků na malé znaky
+Console.WriteLine(txt.ToLower());
+​
+// převod znaků na velké znaky
+string txt = "Šílená čivava";
+Console.WriteLine(txt.ToUpper());*/
+string txt = "cau";
+MorseCodeConverter Jirka = new MorseCodeConverter();
+Console.WriteLine(Jirka.Encode(txt));
+Console.WriteLine(Jirka.normalize(txt));
+Console.WriteLine(Jirka.Decode(".-/.-"));
